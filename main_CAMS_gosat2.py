20f59d8fa50f8866c58adee33c877c2a56778725
Andrew Barr <a.g.barr@sron.nl>
2022-12-12 13:51:40 +0100

    Python 3 compatible main that has been cleaned up and commented.

--- main_CAMS_gosat2.py
+++ main_CAMS_gosat2.py
@@ -8,99 +8,95 @@ import numpy as np
 import subprocess
 from glob import glob
 from datetime import date, timedelta, datetime
-
 import smtplib
-
 from ftplib import FTP, all_errors
-
-# Import the email modules we'll need
 from email.mime.text import MIMEText
 
 
 
-print "STEP 0:Create dir for preprocessing"
+print("Info | STEP 1: Create directories for preprocessing")
 
 version     = '210210'
 dir_nrt     = "/nfs/GOSAT/AUX/OPERATIONAL/CAMS_GOSAT2/"
-dir_gosat2_root   = "/nfs/GOSAT2/FTS/"
 dir_ecmwf   = "/nfs/GOSAT/AUX/OPERATIONAL/CAMS_GOSAT1/ECMWF/ECMWF_data/"
-now         = datetime.now()# initial processing time
+root_path   = "/nfs/GOSAT2/FTS/"
+
+# initial processing time
+now         = datetime.now()
 string_now  = now.isoformat()[0:16]
 
+# days to process: days are today, yesterday and the day before yesterday
+min_step    = [0,-1,-2]
 
-day_step    = [0,1,2]# today, yesterday, the day before yesterday
+# Create directory for new days that don't yet exist to begin the preprocessing
 dates_gosat2 = []
-for item in day_step:
-    date_p   = date.today()-timedelta(days=item)
-#    date_p   = date.today()-timedelta(days=28)
-    dates_gosat2.append(date_p)
-    year     = date_p.year
-    month    = date_p.month
-    day      = date_p.day
+for item in min_step:
 
-    dir_ini = dir_nrt + "PREPROCESS_INI/210210/"+str(year)+"/"+str(month).zfill(2)+"/"+str(day).zfill(2)+"/"
-#    dir_SPOD_ini = dir_nrt + "PREPROCESS_INI/SPOD/220220/"+str(year)+"/"+str(month).zfill(2)+"/"+str(day).zfill(2)+"/"
+    date  = date.today() + timedelta(days=item)
+    dates_gosat2.append(date)
+    year_rt     = date.year
+    month_rt    = date.month
+    day_rt      = date.day
+
+    dir_ini = dir_nrt + "PREPROCESS_INI/210210/"+str(year_rt)+"/"+str(month_rt).zfill(2)+"/"+str(day_rt).zfill(2)+"/"
     if not os.path.exists(dir_ini):
         os.makedirs(dir_ini)
 
 
-for ij, date_p0 in enumerate(dates_gosat2):
-    files_gosat2 = []
+
+
+# Begin processing looping through dates
+for ij, date_d0 in enumerate(dates_gosat2):
+
     start_time  = time.time()
 
-    year  = date_p0.year
-    month = date_p0.month
-    day   = date_p0.day
+    year, month, day  = date_d0.year, date_d0.month, date_d0.day
     date_name = str(year)+str(month).zfill(2)+str(day).zfill(2)
-#   date_name_hour = str(year)+str(month).zfill(2)+str(day).zfill(2)+str(now.hour).zfill(2)
     date_name_hour = str(year)+str(month).zfill(2)+str(day).zfill(2)+string_now.replace(':', '')
-    print 'Processing L1b at:',date_name
-    date_p1      = date_p0+timedelta(days=1)# tormorrow
 
-    year_p1      = date_p1.year
-    month_p1     = date_p1.month
-    day_p1       = date_p1.day
-    date_name_p1 = str(year_p1)+str(month_p1).zfill(2)+str(day_p1).zfill(2)
+    print("Info | Begin Processing of L1b at: ", date_name)
 
-    # ecmwf data
-    dir_ecmwf_ml1 = dir_ecmwf + "ml"+str(date_name)+"_0000.grb"#the day
-    dir_ecmwf_ms1 = dir_ecmwf + "ms"+str(date_name)+"_0000.grb"#
-    dir_ecmwf_ml2 = dir_ecmwf + "ml"+str(date_name)+"_0600.grb"#
-    dir_ecmwf_ms2 = dir_ecmwf + "ms"+str(date_name)+"_0600.grb"#
-    dir_ecmwf_ml3 = dir_ecmwf + "ml"+str(date_name)+"_1200.grb"#
-    dir_ecmwf_ms3 = dir_ecmwf + "ms"+str(date_name)+"_1200.grb"#
-    dir_ecmwf_ml4 = dir_ecmwf + "ml"+str(date_name)+"_1800.grb"#
-    dir_ecmwf_ms4 = dir_ecmwf + "ms"+str(date_name)+"_1800.grb"#
-    dir_ecmwf_ml5 = dir_ecmwf + "ml"+str(date_name_p1)+"_0000.grb"#day after the day
-    dir_ecmwf_ms5 = dir_ecmwf + "ms"+str(date_name_p1)+"_0000.grb"#
+    # Define the day after processing as the preprocessing requires ECMWF data from the current and next day
+    plus_step = 1
+    date_d1      = date_d0+timedelta(days=plus_step)
+    year_d1, month_d1, day_d1 = date_d1.year, date_d1.month, date_d1.day
+    date_name_d1 = str(year_d1)+str(month_d1).zfill(2)+str(day_d1).zfill(2)
+
+    # define ecmwf data from current day
+    ecmwf_ml1 = dir_ecmwf + "ml"+str(date_name)+"_0000.grb"
+    ecmwf_ml2 = dir_ecmwf + "ml"+str(date_name)+"_0600.grb"
+    ecmwf_ml3 = dir_ecmwf + "ml"+str(date_name)+"_1200.grb"
+    ecmwf_ml4 = dir_ecmwf + "ml"+str(date_name)+"_1800.grb"
+    ecmwf_ms1 = dir_ecmwf + "ms"+str(date_name)+"_0000.grb"
+    ecmwf_ms2 = dir_ecmwf + "ms"+str(date_name)+"_0600.grb"
+    ecmwf_ms3 = dir_ecmwf + "ms"+str(date_name)+"_1200.grb"
+    ecmwf_ms4 = dir_ecmwf + "ms"+str(date_name)+"_1800.grb"
+
+    # define ecmwf data from next day
+    ecmwf_ml5 = dir_ecmwf + "ml"+str(date_name_d1)+"_0000.grb"
+    ecmwf_ms5 = dir_ecmwf + "ms"+str(date_name_d1)+"_0000.grb"
+
     # check if ecmwf avaliable
-    ecmwf_flag1 = os.path.exists(dir_ecmwf_ml1) and os.path.exists(dir_ecmwf_ms1)
-    ecmwf_flag2 = os.path.exists(dir_ecmwf_ml2) and os.path.exists(dir_ecmwf_ms2)
-    ecmwf_flag3 = os.path.exists(dir_ecmwf_ml3) and os.path.exists(dir_ecmwf_ms3)
-    ecmwf_flag4 = os.path.exists(dir_ecmwf_ml4) and os.path.exists(dir_ecmwf_ms4)
-    ecmwf_flag5 = os.path.exists(dir_ecmwf_ml5) and os.path.exists(dir_ecmwf_ms5)
+    ecmwf_flag1 = os.path.exists(ecmwf_ml1) and os.path.exists(ecmwf_ms1)
+    ecmwf_flag2 = os.path.exists(ecmwf_ml2) and os.path.exists(ecmwf_ms2)
+    ecmwf_flag3 = os.path.exists(ecmwf_ml3) and os.path.exists(ecmwf_ms3)
+    ecmwf_flag4 = os.path.exists(ecmwf_ml4) and os.path.exists(ecmwf_ms4)
+    ecmwf_flag5 = os.path.exists(ecmwf_ml5) and os.path.exists(ecmwf_ms5)
 
-    # gosat data
-    string_leader_1st = version +"/" + str(year)+"/"+str(month).zfill(2)+"/"+str(day).zfill(2)+"/"
-    string_leader = 'L1B/SWIR_DAY/'  + string_leader_1st
-    dir_gosat2   = dir_gosat2_root   + string_leader
-    files_gosat2 = glob(dir_gosat2+"*.h5")
+    # Create list of level 1 file names to be processed
+    dir_lv1   = root_path + 'L1B/SWIR_DAY/' + version + "/" + str(year)+"/" +str(month).zfill(2)+"/"+str(day).zfill(2)+"/"
+    files_tbp = []  # overwrite file names from previous day
+    files_tbp = glob(dir_lv1+"*.h5")
+    namelist = [x.split(root_path+'L1B/SWIR_DAY/')[1] for x in files_tbp]
 
-    files_full = files_gosat2 # need to exclude processed files
-    files_OBSP = [item.split(dir_gosat2_root+'L1B/SWIR_DAY/')[1] for item in files_full]
-    
-    if len(files_OBSP) == 0 and date_p0 != date.today():
-
-	#there is a delay in downloading the GOSAT2 data which means that there is no data within 24hrs. We need L1X for NRT
-        print 'No GOSAT2 data for the day'
+    if len(namelist) == 0 and date_d0 != date.today():
+        print("Warning | No GOSAT-2 data for today")
         #msg = MIMEText("CRONJOB ISSUE: No GOSAT2 data for the day. Check "+ dir_nrt)
-
         ## me == the sender's email address
         ## you == the recipient's email address
         #msg['Subject'] = 'CRONJOB ISSUE: NO GOSAT2 DATA'
         #msg['From'] = 'a.g.barr@sron.nl'
         #msg['To'] = 'a.g.barr@sron.nl'
-
         ## Send the message via our own SMTP server, but don't include the
         ## envelope header.
         #s = smtplib.SMTP('localhost')
@@ -108,32 +104,22 @@ for ij, date_p0 in enumerate(dates_gosat2):
         #s.quit()
         #continue
 
-    # processed gosat data
-    dir_processed = dir_nrt+'LOG/processed_'+str(date_name)
-    if os.path.exists(dir_processed):
-        with open(dir_processed) as f:
+
+    # Keep log of files that have been processed
+    procfile = dir_nrt+'LOG/processed_'+str(date_name)
+    if os.path.exists(procfile):
+        with open(procfile) as f:
             files_processed = f.read().splitlines()
     else:
             files_processed = []
-    files_GOSAT2 = [x for x in files_OBSP if x not in files_processed]
-    
+    files_GOSAT2 = [x for x in namelist if x not in files_processed]    # avoid multiple processing of files
 
-    if len(files_GOSAT2) == 0:
-        ts          = time.time()
-        st          = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-        files_num   = st + " "+"NUMBER OF PROCESSED L1B FILES = "+str(len(files_processed))+"/"+str(len(files_OBSP))
-        with open(dir_nrt+'LOG/GOSAT_'+str(date_name), "a") as logfile:
-            logfile.write(files_num+"\n")
-        continue
-
-
-    # Two checks --> One of GOSAT L1B and the other for ECMWF data
-    matches   = []
-    for file_name in files_GOSAT2:
-        file_string  = file_name.split("GOSAT2TFTS2")[1]
-
-        hour = int(file_string[8:10])
-        if   hour >=0 and hour < 6:
+    # Check if neccessary ECMWF data avaliable and add to files_processing when true
+    files_processing = []
+    for i in files_GOSAT2:
+        file_string  = i.split("GOSAT2TFTS2")[1]    # parse GOSAT2 level 1 file name
+        hour = int(file_string[8:10])               # the hour is the 9th and 10th digit of the file name
+        if hour >=0 and hour < 6:
             ecmwf_flag = ecmwf_flag1 and ecmwf_flag2
         elif hour >=6  and hour < 12:
             ecmwf_flag = ecmwf_flag2 and ecmwf_flag3
@@ -142,96 +128,79 @@ for ij, date_p0 in enumerate(dates_gosat2):
         elif hour >=18 and hour < 24:
             ecmwf_flag = ecmwf_flag4 and ecmwf_flag5
         else:
-            print "Warning:UNKNOWN TIME for file:",filename
+            print("Warning | UNKNOWN TIME for file:", filename)
             continue
         if ecmwf_flag:
-                matches.append(file_name)
+                files_processing.append(i)
 
-    # If we have no matches, then say something
-        if len(matches) ==0 and len(files_GOSAT2)>0:
-            ts          = time.time()
-            st          = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-            files_num = st + " "+"NUMBER OF UNPROCESSED L1B FILES = "+str(len(files_GOSAT2))+"/"+str(len(files_OBSP))
-            with open(dir_nrt+'LOG/GOSAT_'+str(date_name), "a") as logfile:
-                logfile.write(files_num+"\n")
-            continue
-        else:
-
-            ts          = time.time()
-            st          = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-            files_num = st + " "+"NUMBER OF MATCHED L1B FILES = "+str(len(matches))+"/"+str(len(files_OBSP))
-
+        # Write to log file files_processing: these will be the data that are currently being processed. When they are finished
+        # they will be written to the processed log file
+        ts = time.time()
+        st = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        files_num = st + " "+"NUMBER OF MATCHED L1B FILES = "+str(len(files_processing))+"/"+str(len(namelist))
         with open(dir_nrt+'LOG/GOSAT_'+str(date_name), "a") as logfile:
             logfile.write(files_num+"\n")
-	    
 
 
-    #STEP 0: Check the arriving time of all L1b files
-    last_file = min(matches, key=lambda f:os.path.getctime("{}/{}".format(dir_gosat2_root+'L1B/SWIR_DAY/', f)))
-    file_time = os.stat(dir_gosat2_root+'L1B/SWIR_DAY/'+last_file).st_mtime
-    file_time_string = datetime.fromtimestamp(file_time).strftime("%Y-%m-%d %H:%M:%S")
+    print("Info | STEP 2: Creating list for preprocessing")
+    np.savetxt(dir_nrt+'PREPROCESS/INI/preprocess_'+str(date_name_hour),files_processing, fmt='%s')
 
-    # STEP 1: Creating list (as test)
-    print "STEP 1"
-    np.savetxt(dir_nrt+'PREPROCESS/INI/preprocess_'+str(date_name_hour),matches, fmt='%s')
-    # STEP 3.0: Run preprocessing
-    print "STEP 3"
+
+    print("Info | STEP 3.1: Running preprocessing")
     proc = subprocess.Popen(["./preprocess.sh","1","INI/preprocess_"+str(date_name_hour),str(date_name_hour)],cwd=dir_nrt+'PREPROCESS/')
     proc.wait()
 
-    # STEP 3.1: Store output in subdir
-    print "STEP 3.1"
+
+    print("Info | STEP 3.2: Store output in subdirectory")
     if not os.path.exists(dir_nrt+'PREPROCESS/CONTRL_OUT/'+str(date_name)):
         proc = subprocess.Popen(["mkdir",date_name],cwd=dir_nrt+'PREPROCESS/CONTRL_OUT/')
         proc.wait()
 
 
-    # STEP 3.3: Move output preprocessing to subdir
-    print "STEP 3.3"
+    print("Info | STEP 3.3:  Move output preprocessing to subdirectory")
     proc = subprocess.Popen(["mv","preprocess_id0001.out",str(date_name)+"/"+str(date_name_hour)+".out"],cwd=dir_nrt+'PREPROCESS/CONTRL_OUT/')
     proc.wait()
 
-    # STEP 4: Create Input list for NON-SCATTERING
-    print "STEP 4"
+
+    print("Info | STEP 3.4: Create Input list for proxy retrieval")
     if not os.path.exists(dir_nrt+'PREPROCESS/CONTRL_OUT/'+str(date_name)+'/'+str(date_name_hour)+'.out'):
         continue
-    # STOP AND continue --> WRITE ERROR MESSAGE and add to list
-    proc = subprocess.Popen(["/deos/andreass/anaconda2/bin/python","ascii_to_ascii.py",str(date_name)+'/'+str(date_name_hour)+".out"],cwd = dir_nrt+'PREPROCESS/CONTRL_OUT')
+    proc = subprocess.Popen(["/deos/andrewb/anaconda3/envs/py2/bin/python","ascii_to_ascii.py",str(date_name)+'/'+str(date_name_hour)+".out"],cwd = dir_nrt+'PREPROCESS/CONTRL_OUT')
     proc.wait()
 
-    # STEP 4.5: Move proxy list to directory
-    print "STEP 4.5"
+
+    print("Info | STEP 3.5: Move proxy list to directory")
     proc = subprocess.Popen(["mv","PREPROCESS/CONTRL_OUT/"+str(date_name)+'/'+str(date_name_hour)+".out.db","PROXY/INI/"+str(date_name_hour)],cwd = dir_nrt)
     proc.wait()
-    # STEP 5.0: Run NON-SCATTERING
-    print "STEP 5.0"
+
+
+    print("Info | STEP 4.1: Running proxy retrieval")
     proc = subprocess.Popen(["./proxy_retrieval.sh",str(date_name_hour),str(date_name_hour)],cwd=dir_nrt+'PROXY/')
     proc.wait()
 
 
-    # STEP 5.1: Store output in subdir
-    print "STEP 5.1"
+    print("Info | STEP 4.2:  Store output in subdir")
     if not os.path.exists(dir_nrt+'PROXY/CONTRL_OUT/'+str(date_name)):
         proc = subprocess.Popen(["mkdir",str(date_name)],cwd=dir_nrt+'PROXY/CONTRL_OUT/')
         proc.wait()
 
 
-
-    # STEP 5.3: Change output nonscattering to date.out
-    print "STEP 5.3"
+    print("Info | STEP 4.3: Change name of proxy output")
     proc = subprocess.Popen(["mv","short_id0001.out",str(date_name)+"/"+str(date_name_hour)+".out"],cwd=dir_nrt+'PROXY/CONTRL_OUT/')
     proc.wait()
 
-    # STEP 6: Run RemoTeC to NetCDF script
-    print "STEP 6"
+
+    print("Info | STEP 4.4: Run RemoTeC to NetCDF script")
     proc = subprocess.Popen(["/deos/andrewb/anaconda3/envs/py2/bin/python","PROXY_netcdf_GOSAT2.py","-d "+str(date_name_hour)],cwd = dir_nrt+'PROXY/')
     proc.wait()
 
-    print "STEP 7"#Save processed L1b lists
+
+    print("Info | STEP 5: Save processed L1b lists")
     with open(dir_nrt + 'LOG/processed_'+str(date_name), "a") as logfile:
-        for l1b_file in matches:
+        for l1b_file in files_processing:
             logfile.write(l1b_file+"\n")
+
 
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print elapsed_time
+    print("Info | Time ", elapsed_time)
